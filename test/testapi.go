ba1229ae4f4761d6f137f3bd129a5dc007f82e09
guozi007a <1208001055@qq.com>
2023-08-26 12:08:45 +0800

    配置跨域

--- test/testapi.go
+++ test/testapi.go
@@ -3,11 +3,20 @@ package test
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Testapi() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowMethods:     []string{"GET", "POST", "OPTIONS", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Cookie"},
+		AllowCredentials: true,
+		AllowOrigins:     []string{"*"},
+		MaxAge:           20,
+	}))
 
 	v1 := r.Group("/v1")
 	{
