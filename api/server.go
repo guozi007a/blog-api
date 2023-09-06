edc3d43369d89d142e57fa4b4516f9968ee38b07
guozi007a <1208001055@qq.com>
2023-09-07 07:16:06 +0800

    重新设置maxage

--- api/server.go
+++ api/server.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -13,7 +15,7 @@ func Server() {
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Cookie"},
 		AllowCredentials: true,
 		AllowOrigins:     []string{"*"},
-		MaxAge:           20,
+		MaxAge:           24 * time.Hour, // 表示在24小时内，同样的预检请求可以不再重复进行了
 	}))
 
 	groupRouter(r)
