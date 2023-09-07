f73e0981c6c35d5bc791a8d2e7e50c7c251586fb
wushijie <wushijie@melot.cn>
2023-09-07 15:41:19 +0800

    更换dev配置

--- db_server/config.go
+++ db_server/config.go
@@ -7,9 +7,8 @@ import (
 var (
 	user     = "root" //用户名
 	password = "123456"
-	// password = "jL9@xZ2!tQ6*kT1)>D5+nA^|lN4~zF5$" // 密码
-	// host     = "127.0.0.1"                                // 域名
-	host   = "mysql"
+	host     = "localhost" // dev域名
+	// host   = "mysql" // prod域名 跟yml中mysql名称保持一致
 	port   = "3306"                                     // 数据库端口
 	dbname = "vm50"                                     //数据库名称
 	extra  = "charset=utf8mb4&parseTime=True&loc=Local" // 其他配置
