fabca4235da3638ae487c6e4c1a6359a2f7bdf09
guozi007a <1208001055@qq.com>
2023-09-09 22:48:36 +0800

    uploading...

--- api/backstage/resource.go
+++ api/backstage/resource.go
@@ -7,6 +7,12 @@ import (
 	// "gorm.io/gorm/clause"
 )
 
+// 文件直传
+func UploadFileDirect(c *gin.Context) {
+	// db := global.GlobalDB
+
+}
+
 // 图片预传
 func PreUpload(c *gin.Context) {
 	// db := global.GlobalDB
