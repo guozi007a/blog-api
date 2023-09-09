85f0e3f7b3d4e5afe7f0eab71631a4335fd38d01
guozi007a <1208001055@qq.com>
2023-09-09 15:31:37 +0800

    resource start

---
+++ api/backstage/resource.go
@@ -0,0 +1,14 @@
+package backstage
+
+import (
+	// "net/http"
+	// "blog-api/global"
+	"github.com/gin-gonic/gin"
+	// "gorm.io/gorm/clause"
+)
+
+// 图片预传
+func PreUpload(c *gin.Context) {
+	// db := global.GlobalDB
+
+}
