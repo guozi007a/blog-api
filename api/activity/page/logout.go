f199975d8fcec482a4f22dc9bc7b6ce0ca1047bf
wushijie <wushijie@melot.cn>
2023-11-29 15:17:03 +0800

    add logout api

---
+++ api/activity/page/logout.go
@@ -0,0 +1,34 @@
+package page
+
+import (
+	"net/http"
+
+	"blog-api/db_server/tables"
+	"blog-api/global"
+
+	"github.com/gin-gonic/gin"
+)
+
+type LogoutInfo struct {
+	UserId int `json:"userId"`
+}
+
+func Logout(c *gin.Context) {
+	db := global.GlobalDB
+
+	var info LogoutInfo
+	err := c.ShouldBind(&info)
+	if err != nil {
+		panic(err)
+	}
+	var idInfo tables.IdInfo
+	result := db.Model(&idInfo).Where("userId = ?", info.UserId).Select("isLogin", "token").Updates(tables.IdInfo{IsLogin: false, Token: ""})
+	if result.Error != nil {
+		panic(result.Error)
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code":    global.CodeOK,
+		"message": "success",
+		"data":    true,
+	})
+}
