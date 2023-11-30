15834a2d86d0c08bd6c633b8fa2ac134c1fae744
wushijie <wushijie@melot.cn>
2023-11-30 18:31:48 +0800

    调试cookie

--- api/activity/page/logout.go
+++ api/activity/page/logout.go
@@ -24,10 +24,15 @@ func Logout(c *gin.Context) {
 	var idInfo tables.IdInfo
 	result := db.Model(&idInfo).Where("userId = ?", info.UserId).Select("isLogin", "token").Updates(tables.IdInfo{IsLogin: false, Token: ""})
 	if result.Error != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    global.CodeNotModify,
+			"message": "退出失败",
+			"data":    nil,
+		})
 		panic(result.Error)
 	}
-	c.SetCookie("activityUserId", "", -1, "/", "", false, true)
-	c.SetCookie("activityToken", "", -1, "/", "", false, true)
+	c.SetCookie("activityUserId", "", -1, "/", global.ActivityCookieAllowOrigin(), false, true)
+	c.SetCookie("activityToken", "", -1, "/", global.ActivityCookieAllowOrigin(), false, true)
 	c.JSON(http.StatusOK, gin.H{
 		"code":    global.CodeOK,
 		"message": "success",
--- api/activity/page/profile_info.go
+++ api/activity/page/profile_info.go
@@ -1,6 +1,7 @@
 package page
 
 import (
+	"fmt"
 	"net/http"
 
 	"blog-api/db_server/tables"
@@ -21,6 +22,10 @@ func GetProfileInfo(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{})
 		return
 	}
+	// if cookieUserId == "10323" {
+	// 	c.JSON(http.StatusOK, gin.H{})
+	// 	return
+	// }
 	cookieToken, err := c.Cookie("activityToken")
 	if err != nil {
 		c.JSON(http.StatusOK, gin.H{})
@@ -33,6 +38,7 @@ func GetProfileInfo(c *gin.Context) {
 	var userInfo tables.IdInfo
 	uid, err := strconv.Atoi(cookieUserId)
 	if err != nil {
+		c.JSON(http.StatusOK, gin.H{})
 		panic(err)
 	}
 	claims := plugins.ParseToken(cookieToken)
@@ -40,16 +46,22 @@ func GetProfileInfo(c *gin.Context) {
 	now := time.Now().Unix()
 	// token过期了
 	if now > expTime {
+		fmt.Println("token过期了")
 		db.Model(&userInfo).Where("userId = ? AND token = ?", uid, cookieToken).Select("isLogin", "token").Updates(tables.IdInfo{IsLogin: false, Token: ""})
+		c.SetCookie("activityUserId", "", -1, "/", global.ActivityCookieAllowOrigin(), false, true)
+		c.SetCookie("activityToken", "", -1, "/", global.ActivityCookieAllowOrigin(), false, true)
 		c.JSON(http.StatusOK, gin.H{})
 		return
 	} else {
+		fmt.Println("token没过期")
 		db.Model(&userInfo).Where("userId = ? AND token = ?", uid, cookieToken).Update("isLogin", true)
+		fmt.Printf("uid: %v, cookieToken: %s\n", uid, cookieToken)
 		result := db.Table(userInfo.TableName()).Where("userId = ? AND token = ?", uid, cookieToken).Find(&userInfo)
 		if result.Error != nil {
 			c.JSON(http.StatusOK, gin.H{})
 			panic(result.Error)
 		}
+		fmt.Printf("id: %v, token: %s\n", userInfo.UserId, userInfo.Token)
 		c.JSON(http.StatusOK, gin.H{
 			"code":    global.CodeOK,
 			"message": "success",
