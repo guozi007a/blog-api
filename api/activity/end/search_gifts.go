d0f5c7eec5796fbf852c2355d8096abb42c73e45
wushijie <wushijie@melot.cn>
2023-12-20 18:05:25 +0800

    5

---
+++ api/activity/end/search_gifts.go
@@ -0,0 +1,64 @@
+package end
+
+import (
+	"blog-api/db_server/tables"
+	"blog-api/global"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type SearchGiftsQuery struct {
+	GiftId       int    `json:"giftId" form:"giftId"`
+	GiftName     string `json:"giftName" form:"giftName"`
+	GiftTypeID   int    `json:"giftTypeId" form:"giftTypeId"`
+	ExtendsID    int    `json:"extendsId" form:"extendsId"`
+	GiftTagID    int    `json:"giftTagId" form:"giftTagId"`
+	MinGiftValue int64  `json:"minGiftValue" form:"minGiftValue"`
+	MaxGiftValue int64  `json:"maxGiftValue" form:"maxGiftValue"`
+	Page         int    `json:"page" form:"page"`
+	PageSize     int    `json:"pageSize" form:"pageSize"`
+}
+
+func SearchGifts(c *gin.Context) {
+	db := global.GlobalDB
+
+	var params SearchGiftsQuery
+	err := c.ShouldBindQuery(&params)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    global.CodeGetParamsFailed,
+			"message": "参数解析失败",
+			"data":    nil,
+		})
+		return
+	}
+	if params.Page == 0 || params.PageSize == 0 {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    global.CodeLackRequired,
+			"message": "参数解析失败",
+			"data":    nil,
+		})
+		return
+	}
+
+	var gifts []tables.KKGifts
+	if params.GiftId == 0 && params.GiftName == "" && params.GiftTypeID == 0 && params.ExtendsID == 0 && params.GiftTagID == 0 && params.MinGiftValue == 0 && params.MaxGiftValue == 0 { /* 搜索全部 */
+		result := db.Model(&tables.KKGifts{}).Order("createDate desc").Limit(params.PageSize).Offset((params.Page - 1) * params.PageSize).Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code":    global.CodeOK,
+		"message": "success",
+		"data":    gifts,
+	})
+	return
+}
