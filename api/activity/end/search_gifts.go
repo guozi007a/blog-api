89ab80245105ed7d88185d4f4ed46c1b3c67cea5
wushijie <wushijie@melot.cn>
2023-12-21 18:22:01 +0800

    gift查询api

--- api/activity/end/search_gifts.go
+++ api/activity/end/search_gifts.go
@@ -3,6 +3,7 @@ package end
 import (
 	"blog-api/db_server/tables"
 	"blog-api/global"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -68,6 +69,368 @@ func SearchGifts(c *gin.Context) {
 			})
 			return
 		}
+	} else if params.GiftId != 0 && params.GiftName == "" && params.GiftTypeID == 0 && params.ExtendsID == 0 && params.GiftTagID == 0 && params.MinGiftValue == 0 && params.MaxGiftValue == 0 { /* 按礼物id查找，此时不需要分页了，毕竟礼物id是唯一的，要么查不到，要么只能查到一条 */
+		result := db.Model(&tables.KKGifts{}).Where("giftId = ?", params.GiftId).Order("createDate desc").Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		count = int64(len(gifts))
+	} else if params.GiftId == 0 && params.GiftName != "" && params.GiftTypeID == 0 && params.ExtendsID == 0 && params.GiftTagID == 0 && params.MinGiftValue == 0 && params.MaxGiftValue == 0 { /* 按礼物名称模糊匹配 */
+		result := db.Model(&tables.KKGifts{}).Where("giftName Like ?", fmt.Sprintf("%%%s%%", params.GiftName)).Order("createDate desc").Limit(params.PageSize).Offset((params.Page - 1) * params.PageSize).Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		result = db.Model(&tables.KKGifts{}).Where("giftName Like ?", fmt.Sprintf("%%%s%%", params.GiftName)).Count(&count)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+	} else if params.GiftId == 0 && params.GiftName == "" && params.GiftTypeID != 0 && params.ExtendsID == 0 && params.GiftTagID == 0 && params.MinGiftValue == 0 && params.MaxGiftValue == 0 { /* 按礼物类型查找 */
+		result := db.Model(&tables.KKGifts{}).Where("giftTypeId = ?", params.GiftTypeID).Order("createDate desc").Limit(params.PageSize).Offset((params.Page - 1) * params.PageSize).Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		result = db.Model(&tables.KKGifts{}).Where("giftTypeId = ?", params.GiftTypeID).Count(&count)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+	} else if params.GiftId == 0 && params.GiftName == "" && params.GiftTypeID == 0 && params.ExtendsID != 0 && params.GiftTagID == 0 && params.MinGiftValue == 0 && params.MaxGiftValue == 0 { /* 按礼物扩展类型查找 */
+		/* 这里适用于外连接查询 */
+		result := db.Model(&tables.KKGifts{}).Joins("left join extends_type on kk_gifts.giftId = extends_type.giftId and extends_type.extendsId = ?", params.ExtendsID).Order("createDate desc").Limit(params.PageSize).Offset((params.Page - 1) * params.PageSize).Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		result = db.Model(&tables.ExtendsType{}).Where("extendsId = ?", params.ExtendsID).Count(&count)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+	} else if params.GiftId == 0 && params.GiftName == "" && params.GiftTypeID == 0 && params.ExtendsID == 0 && params.GiftTagID != 0 && params.MinGiftValue == 0 && params.MaxGiftValue == 0 { /* 按礼物标签查找 */
+		result := db.Model(&tables.KKGifts{}).Joins("left join gift_tag on kk_gifts.giftId = gift_tag.giftId and gift_tag.giftTagId = ?", params.GiftTagID).Order("createDate desc").Limit(params.PageSize).Offset((params.Page - 1) * params.PageSize).Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		result = db.Model(&tables.ExtendsType{}).Where("giftTagId = ?", params.GiftTagID).Count(&count)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+	} else if params.GiftId == 0 && params.GiftName == "" && params.GiftTypeID == 0 && params.ExtendsID == 0 && params.GiftTagID == 0 && params.MaxGiftValue != 0 { /* 按礼物价值区间查找 */
+		result := db.Model(&tables.KKGifts{}).Where("giftValue BETWEEN ? AND ?", params.MinGiftValue, params.MaxGiftValue).Order("createDate desc").Limit(params.PageSize).Offset((params.Page - 1) * params.PageSize).Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		result = db.Model(&tables.KKGifts{}).Where("giftValue BETWEEN ? AND ?", params.MinGiftValue, params.MaxGiftValue).Count(&count)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+	} else if params.GiftId != 0 && params.GiftName != "" && params.GiftTypeID == 0 && params.ExtendsID == 0 && params.GiftTagID != 0 && params.MinGiftValue == 0 && params.MaxGiftValue == 0 { /* 按礼物id和礼物名称查找 */
+		result := db.Model(&tables.KKGifts{}).Where("giftId = ? AND giftName Like ?", params.GiftId, fmt.Sprintf("%%%s%%", params.GiftName)).Order("createDate desc").Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		count = int64(len(gifts))
+	} else if params.GiftId != 0 && params.GiftName == "" && params.GiftTypeID != 0 && params.ExtendsID == 0 && params.GiftTagID != 0 && params.MinGiftValue == 0 && params.MaxGiftValue == 0 { /* 按礼物id和礼物类型查找 */
+		result := db.Model(&tables.KKGifts{}).Where("giftId = ? AND giftTypeId = ?", params.GiftId, params.GiftTypeID).Order("createDate desc").Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		count = int64(len(gifts))
+	} else if params.GiftId != 0 && params.GiftName == "" && params.GiftTypeID == 0 && params.ExtendsID != 0 && params.GiftTagID == 0 && params.MinGiftValue == 0 && params.MaxGiftValue == 0 { /* 按礼物id和礼物扩展类型查找 */
+		result := db.Model(&tables.KKGifts{}).Where("giftId = ?", params.GiftId).Joins("left join extends_type on kk_gifts.giftId = extends_type.giftId and extends_type.extendsId = ?", params.ExtendsID).Order("createDate desc").Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		count = int64(len(gifts))
+	} else if params.GiftId != 0 && params.GiftName == "" && params.GiftTypeID == 0 && params.ExtendsID == 0 && params.GiftTagID != 0 && params.MinGiftValue == 0 && params.MaxGiftValue == 0 { /* 按礼物id和礼物标签查找 */
+		result := db.Model(&tables.KKGifts{}).Where("giftId = ?", params.GiftId).Joins("left join gift_tag on kk_gifts.giftId = gift_tag.giftId and gift_tag.giftTagId = ?", params.GiftTagID).Order("createDate desc").Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		count = int64(len(gifts))
+	} else if params.GiftId != 0 && params.GiftName == "" && params.GiftTypeID == 0 && params.ExtendsID == 0 && params.GiftTagID == 0 && params.MaxGiftValue != 0 { /* 按礼物id和礼物价值区间查找 */
+		result := db.Model(&tables.KKGifts{}).Where("giftId = ? AND giftValue BETWEEN ? AND ?", params.GiftId, params.MinGiftValue, params.MaxGiftValue).Order("createDate desc").Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		count = int64(len(gifts))
+	} else if params.GiftId == 0 && params.GiftName != "" && params.GiftTypeID != 0 && params.ExtendsID == 0 && params.GiftTagID == 0 && params.MinGiftValue == 0 && params.MaxGiftValue == 0 { /* 按礼物名称和礼物类型匹配 */
+		result := db.Model(&tables.KKGifts{}).Where("giftName Like ? AND giftTypeId = ?", fmt.Sprintf("%%%s%%", params.GiftName), params.GiftTypeID).Order("createDate desc").Limit(params.PageSize).Offset((params.Page - 1) * params.PageSize).Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		result = db.Model(&tables.KKGifts{}).Where("giftName Like ? AND giftTypeId = ?", fmt.Sprintf("%%%s%%", params.GiftName), params.GiftTypeID).Count(&count)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+	} else if params.GiftId == 0 && params.GiftName != "" && params.GiftTypeID == 0 && params.ExtendsID != 0 && params.GiftTagID == 0 && params.MinGiftValue == 0 && params.MaxGiftValue == 0 { /* 按礼物名称和礼物扩展类型查找 */
+		result := db.Model(&tables.KKGifts{}).Where("giftName Like ?", fmt.Sprintf("%%%s%%", params.GiftName)).Joins("left join extends_type on kk_gifts.giftId = extends_type.giftId and extends_type.extendsId = ?", params.ExtendsID).Order("createDate desc").Limit(params.PageSize).Offset((params.Page - 1) * params.PageSize).Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		result = db.Model(&tables.KKGifts{}).Where("giftName Like ?", fmt.Sprintf("%%%s%%", params.GiftName)).Joins("left join extends_type on kk_gifts.giftId = extends_type.giftId and extends_type.extendsId = ?", params.ExtendsID).Count(&count)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+	} else if params.GiftId == 0 && params.GiftName != "" && params.GiftTypeID == 0 && params.ExtendsID == 0 && params.GiftTagID != 0 && params.MinGiftValue == 0 && params.MaxGiftValue == 0 { /* 按礼物名称和礼物标签查找 */
+		result := db.Model(&tables.KKGifts{}).Where("giftName Like ?", fmt.Sprintf("%%%s%%", params.GiftName)).Joins("left join gift_tag on kk_gifts.giftId = gift_tag.giftId and gift_tag.giftTagId = ?", params.GiftTagID).Order("createDate desc").Limit(params.PageSize).Offset((params.Page - 1) * params.PageSize).Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		result = db.Model(&tables.KKGifts{}).Where("giftName Like ?", fmt.Sprintf("%%%s%%", params.GiftName)).Joins("left join gift_tag on kk_gifts.giftId = gift_tag.giftId and gift_tag.giftTagId = ?", params.GiftTagID).Count(&count)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+	} else if params.GiftId == 0 && params.GiftName != "" && params.GiftTypeID == 0 && params.ExtendsID == 0 && params.GiftTagID == 0 && params.MaxGiftValue != 0 { /* 按礼物名称和礼物价值区间查找 */
+		result := db.Model(&tables.KKGifts{}).Where("giftName Like ? AND giftValue BETWEEN ? AND ?", fmt.Sprintf("%%%s%%", params.GiftName), params.MinGiftValue, params.MaxGiftValue).Order("createDate desc").Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		result = db.Model(&tables.KKGifts{}).Where("giftName Like ? AND giftValue BETWEEN ? AND ?", fmt.Sprintf("%%%s%%", params.GiftName), params.MinGiftValue, params.MaxGiftValue).Count(&count)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+	} else if params.GiftId == 0 && params.GiftName == "" && params.GiftTypeID != 0 && params.ExtendsID != 0 && params.GiftTagID == 0 && params.MinGiftValue == 0 && params.MaxGiftValue == 0 { /* 按礼物类型和礼物扩展类型查找 */
+		result := db.Model(&tables.KKGifts{}).Where("giftTypeId = ", params.GiftTypeID).Joins("left join extends_type on kk_gifts.giftId = extends_type.giftId and extends_type.extendsId = ?", params.ExtendsID).Order("createDate desc").Limit(params.PageSize).Offset((params.Page - 1) * params.PageSize).Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		result = db.Model(&tables.KKGifts{}).Where("giftTypeId = ", params.GiftTypeID).Joins("left join extends_type on kk_gifts.giftId = extends_type.giftId and extends_type.extendsId = ?", params.ExtendsID).Count(&count)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+	} else if params.GiftId == 0 && params.GiftName == "" && params.GiftTypeID != 0 && params.ExtendsID == 0 && params.GiftTagID != 0 && params.MinGiftValue == 0 && params.MaxGiftValue == 0 { /* 按礼物类型和礼物标签查找 */
+		result := db.Model(&tables.KKGifts{}).Where("giftTypeId = ", params.GiftTypeID).Joins("left join gift_tag on kk_gifts.giftId = gift_tag.giftId and gift_tag.giftTagId = ?", params.GiftTagID).Order("createDate desc").Limit(params.PageSize).Offset((params.Page - 1) * params.PageSize).Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		result = db.Model(&tables.KKGifts{}).Where("giftTypeId = ", params.GiftTypeID).Joins("left join gift_tag on kk_gifts.giftId = gift_tag.giftId and gift_tag.giftTagId = ?", params.GiftTagID).Count(&count)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+	} else if params.GiftId == 0 && params.GiftName == "" && params.GiftTypeID != 0 && params.ExtendsID == 0 && params.GiftTagID == 0 && params.MaxGiftValue != 0 { /* 按礼物类型和礼物价值区间查找 */
+		result := db.Model(&tables.KKGifts{}).Where("giftTypeId = ? AND giftValue BETWEEN ? AND ?", params.GiftTypeID, params.MinGiftValue, params.MaxGiftValue).Order("createDate desc").Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		result = db.Model(&tables.KKGifts{}).Where("giftTypeId = ? AND giftValue BETWEEN ? AND ?", params.GiftTypeID, params.MinGiftValue, params.MaxGiftValue).Count(&count)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+	} else if params.GiftId == 0 && params.GiftName == "" && params.GiftTypeID == 0 && params.ExtendsID != 0 && params.GiftTagID != 0 && params.MinGiftValue == 0 && params.MaxGiftValue == 0 { /* 按礼物扩展类型和礼物标签查找 */
+		result := db.Model(&tables.KKGifts{}).Joins("left join extends_type on kk_gifts.giftId = extends_type.giftId and extends_type.extendsId = ?", params.ExtendsID).Joins("left join gift_tag on kk_gifts.giftId = gift_tag.giftId and gift_tag.giftTagId = ?", params.GiftTagID).Order("createDate desc").Limit(params.PageSize).Offset((params.Page - 1) * params.PageSize).Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		result = db.Model(&tables.KKGifts{}).Joins("left join extends_type on kk_gifts.giftId = extends_type.giftId and extends_type.extendsId = ?", params.ExtendsID).Joins("left join gift_tag on kk_gifts.giftId = gift_tag.giftId and gift_tag.giftTagId = ?", params.GiftTagID).Count(&count)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+	} else if params.GiftId == 0 && params.GiftName == "" && params.GiftTypeID == 0 && params.ExtendsID != 0 && params.GiftTagID == 0 && params.MaxGiftValue != 0 { /* 按礼物拓展和礼物价值区间查找 */
+		result := db.Model(&tables.KKGifts{}).Where("giftValue BETWEEN ? AND ?", params.MinGiftValue, params.MaxGiftValue).Joins("left join extends_type on kk_gifts.giftId = extends_type.giftId and extends_type.extendsId = ?", params.ExtendsID).Order("createDate desc").Limit(params.PageSize).Offset((params.Page - 1) * params.PageSize).Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		result = db.Model(&tables.KKGifts{}).Where("giftValue BETWEEN ? AND ?", params.MinGiftValue, params.MaxGiftValue).Joins("left join extends_type on kk_gifts.giftId = extends_type.giftId and extends_type.extendsId = ?", params.ExtendsID).Count(&count)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+	} else if params.GiftId == 0 && params.GiftName == "" && params.GiftTypeID == 0 && params.ExtendsID == 0 && params.GiftTagID != 0 && params.MaxGiftValue != 0 { /* 按礼物标签和礼物价值区间查找 */
+		result := db.Model(&tables.KKGifts{}).Where("giftValue BETWEEN ? AND ?", params.MinGiftValue, params.MaxGiftValue).Joins("left join gift_tag on kk_gifts.giftId = gift_tag.giftId and gift_tag.giftTagId = ?", params.GiftTagID).Order("createDate desc").Limit(params.PageSize).Offset((params.Page - 1) * params.PageSize).Preload("GiftTags").Preload("ExtendsTypes").Find(&gifts)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+		result = db.Model(&tables.KKGifts{}).Where("giftValue BETWEEN ? AND ?", params.MinGiftValue, params.MaxGiftValue).Joins("left join gift_tag on kk_gifts.giftId = gift_tag.giftId and gift_tag.giftTagId = ?", params.GiftTagID).Count(&count)
+		if result.Error != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    global.CodeQueryFailed,
+				"message": "查询失败",
+				"data":    nil,
+			})
+			return
+		}
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    global.CodeQueryFailed,
+			"message": "暂不支持同时3种及以上的查询条件！",
+			"data": SearchGiftsRes{
+				GiftList: gifts,
+				Total:    count,
+			},
+		})
+		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
@@ -78,5 +441,4 @@ func SearchGifts(c *gin.Context) {
 			Total:    count,
 		},
 	})
-	return
 }
--- db_server/tables/gifts.go
+++ db_server/tables/gifts.go
@@ -21,20 +21,18 @@ func (ExtendsType) TableName() string {
 }
 
 type KKGifts struct {
-	GiftID       int           `json:"giftId" gorm:"primaryKey;comment:礼物ID;column:giftId"`
-	GiftName     string        `json:"giftName" gorm:"comment:礼物名称;column:giftName"`
-	GiftType     string        `json:"giftType" gorm:"comment:礼物分类，如促销礼物，高级礼物，豪华礼物等;column:giftType"`
-	GiftTypeID   int           `json:"giftTypeId" gorm:"comment:不同类别的礼物对应的不同ID;column:giftTypeId"`
-	ExtendsTypes []ExtendsType `json:"extendsTypes" gorm:"foreignKey:GiftID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
-	GiftTags     []GiftTag     `json:"giftTags" gorm:"foreignKey:GiftID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
-	// ExtendsTypes   []ExtendsType `json:"extendsTypes" gorm:"-"`
-	// GiftTags       []GiftTag     `json:"giftTags" gorm:"-"`
-	GiftValue      int64  `json:"giftValue" gorm:"comment:礼物价格(秀币);column:giftValue"`
-	CreateDate     int64  `json:"createDate" gorm:"comment:创建该记录的时间戳;autoCreateTime:milli;column:createDate"`
-	UpdateDate     int64  `json:"updateDate" gorm:"comment:更新记录的时间戳;autoUpdateTime:milli;column:updateDate"`
-	GiftDescribe   string `json:"giftDescribe" gorm:"comment:礼物描述;column:giftDescribe"`
-	CornerMarkID   int    `json:"cornerMarkId" gorm:"comment:角标类型;column:cornerMarkId"`
-	CornerMarkName string `json:"cornerMarkName" gorm:"comment:角标名称;column:cornerMarkName"`
+	GiftID         int           `json:"giftId" gorm:"primaryKey;comment:礼物ID;column:giftId"`
+	GiftName       string        `json:"giftName" gorm:"comment:礼物名称;column:giftName"`
+	GiftType       string        `json:"giftType" gorm:"comment:礼物分类，如促销礼物，高级礼物，豪华礼物等;column:giftType"`
+	GiftTypeID     int           `json:"giftTypeId" gorm:"comment:不同类别的礼物对应的不同ID;column:giftTypeId"`
+	ExtendsTypes   []ExtendsType `json:"extendsTypes" gorm:"foreignKey:GiftID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
+	GiftTags       []GiftTag     `json:"giftTags" gorm:"foreignKey:GiftID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
+	GiftValue      int64         `json:"giftValue" gorm:"comment:礼物价格(秀币);column:giftValue"`
+	CreateDate     int64         `json:"createDate" gorm:"comment:创建该记录的时间戳;autoCreateTime:milli;column:createDate"`
+	UpdateDate     int64         `json:"updateDate" gorm:"comment:更新记录的时间戳;autoUpdateTime:milli;column:updateDate"`
+	GiftDescribe   string        `json:"giftDescribe" gorm:"comment:礼物描述;column:giftDescribe"`
+	CornerMarkID   int           `json:"cornerMarkId" gorm:"comment:角标类型;column:cornerMarkId"`
+	CornerMarkName string        `json:"cornerMarkName" gorm:"comment:角标名称;column:cornerMarkName"`
 }
 
 func (KKGifts) TableName() string {
